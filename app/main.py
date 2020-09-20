51cf80481a877d8a02d32f8f62ac3c23daf677b9
vincentyliu2001 <vincent.y.liu.23@dartmouth.edu>
2020-09-20 03:52:42 -0500

    testing

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from flask import Flask 
-  
-app = Flask(__name__) 
-  
-@app.route("/") 
-def home_view(): 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
+@app.route('/api/filterAlgo', methods=['GET', 'POST'])
+def query_example():
+    if not request.json :
+        return 'Please Pass a Valid JSON Object'
+    else:
+        return 'apply(request.json)'
